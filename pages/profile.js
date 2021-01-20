7f074109d60b693eba2d0008483b637aba9cefe5
Mohammad Shahbaz Alam <shahbaz@magic.link>
2021-01-20 18:47:41 +0530

    fixes

--- pages/profile.js
+++ pages/profile.js
@@ -1,19 +1,21 @@
-import { useUser } from '../lib/hooks';
-import Layout from '../components/layout';
+import { useUser } from '../lib/hooks'
+import Layout from '../components/layout'
 
 const Profile = () => {
-  const user = useUser({ redirectTo: '/login' });
+	const user = useUser({ redirectTo: '/login' })
 
-  return (
-    <Layout>
-      {user && (
-        <>
-          <h2>Profile</h2>
-          <pre style={{ overflow: 'auto' }}>{JSON.stringify(user, null, 2)}</pre>
-        </>
-      )}
-    </Layout>
-  );
-};
+	return (
+		<Layout>
+			{user && (
+				<>
+					<h2>Profile</h2>
+					<pre style={{ overflow: 'auto' }}>
+						{JSON.stringify(user, null, 2)}
+					</pre>
+				</>
+			)}
+		</Layout>
+	)
+}
 
-export default Profile;
+export default Profile
