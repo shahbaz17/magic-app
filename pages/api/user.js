41a02d802b20cd17ab421405e6ffcc8f8b6c6a71
Mohammad Shahbaz Alam <shahbaz@magic.link>
2021-01-20 18:50:54 +0530

    fixes

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,10 +1,10 @@
-import { getSession } from '../../lib/iron';
+import { getSession } from '../../lib/iron'
 
 export default async function user(req, res) {
-  try {
-    const session = await getSession(req);
-    res.status(200).json({ user: session || null });
-  } catch (error) {
-    res.status(200).json({ user: null });
-  }
+	try {
+		const session = await getSession(req)
+		res.status(200).json({ user: session || null })
+	} catch (error) {
+		res.status(200).json({ user: null })
+	}
 }
